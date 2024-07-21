59ab6c4cf5038fb8eeac35bc79594c3aced58377
M.ARAVIND <116102105+AravindHacker@users.noreply.github.com>
2024-07-21 22:19:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { AuthProvider } from './Components/AuthContext';
 import FirstLogin from './Components/FirstLogin';
